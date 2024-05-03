88bcf6cc7c744dfc61c16b5d5b200d063fb294e0
darkkazma <darkkazma@solbox.com>
2024-05-03 23:20:02 +0900

    init test

---
+++ components/shared/LoadingLogo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+type Props = {
+  size?: number;
+}
+const LoadingLogo = ({size = 100}: Props) => {
+  return <div className="h-full w-full flex justify-center items-center">
+    <Image src="/logo.svg" alt="Logo" width={size} height={size} className="animate-pulse duration-80" />
+  </div>;
+}
+
+export default LoadingLogo;
